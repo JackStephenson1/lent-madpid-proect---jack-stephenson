ec1cbbd1e83716590135b9d66223feb1a7f43eea
JackStephenson1 <77438054+JackStephenson1@users.noreply.github.com>
2021-02-10 13:43:36 +0000

    Quick save

--- main.ts
+++ main.ts
@@ -1,34 +1,31 @@
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (true) {
-        mySprite.vy = -200
-        mySprite.y += -75
-    } else {
-        mySprite.vy = 200
+    if (mySprite.vy == 0) {
+        mySprite.vy = -300
     }
 })
 controller.right.onEvent(ControllerButtonEvent.Repeated, function () {
     mySprite.x += 5
 })
 info.onCountdownEnd(function () {
-    projectile = sprites.createProjectileFromSprite(img`
+    projectile = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . f f f f f . . . . . . 
+        . . . . f f . . . . f f . . . . 
+        . . . . f . . f f f . f f . . . 
+        . . . . f . f . f f . . f . . . 
+        . . . . f f . f f f . . f . . . 
+        . . . . f f . f f f f f f . . . 
+        . . . . f f . f f f . f f . . . 
+        . . . . f f . . f f f . . . . . 
+        . . . . f f . . f f f . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . . f f f f f f . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
-        . . . . . e e e e . . . 1 1 . . 
-        . . . . . f d d f . . 1 . . . . 
-        . . . . . d d d d . 1 . . . . . 
-        . 1 . . . . d d . . 1 . . . . . 
-        . . 1 1 1 1 1 1 1 1 . . . . . . 
-        . . . . . . 1 1 . . . . . . . . 
-        . . . . . . 1 1 . . . . . . . . 
-        . . . . . . f f . . . . . . . . 
-        . . . . . f . . f . . . . . . . 
-        . . . . . f . . f . . . . . . . 
-        . . . . . f . . f . . . . . . . 
-        . . . f f f . . f f f . . . . . 
         . . . . . . . . . . . . . . . . 
-        `, mySprite, 5, 5)
-    projectile.setPosition(75, 105)
+        `, SpriteKind.Projectile)
+    projectile.setPosition(76, 86)
     projectile.follow(mySprite)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
@@ -39,29 +36,32 @@ controller.left.onEvent(ControllerButtonEvent.Repeated, function () {
 })
 let projectile: Sprite = null
 let mySprite: Sprite = null
-tiles.setTilemap(tilemap`level1`)
-info.startCountdown(10)
 mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . 2 . . . . . . 2 . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . 2 . . . . . . 2 . . . . 
-    . . . . 2 2 . . . . . 2 . . . . 
-    . . . . . 2 2 . . 2 2 2 . . . . 
-    . . . . . . 2 2 2 2 . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
+    . d . . . . . . . . . . . . d . 
+    d d d . . . e e e e . . . d d d 
+    . d f . . . d d d d . . . f d . 
+    . f f f . . d d d d . . f f f . 
+    . . f f f . 1 d d 1 . f f . . . 
+    . . . . f f 1 1 1 1 f f . . . . 
+    . . . . . f f 1 1 f f . . . . . 
+    . . . . . f f 1 1 f f . . . . . 
+    . . . . . f f 1 1 f f . . . . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    . . . f f f f . . f f f f . . . 
+    . . f f f f f . . f f f f f . . 
     `, SpriteKind.Player)
+tiles.setTilemap(tilemap`level3`)
+mySprite.say("You have escaped JWFS's Dull Computer Science lesson!", 5000)
+pause(5000)
+mySprite.say("Now climb the different Eton landmarks before he can catch you!", 5000)
+pause(5000)
+tiles.setTilemap(tilemap`level1`)
+info.startCountdown(30)
 mySprite.setPosition(4, 46)
 mySprite.setFlag(SpriteFlag.StayInScreen, true)
 scene.cameraFollowSprite(mySprite)
-forever(function () {
-    mySprite.vy = 200
-})
+mySprite.ay = 450
